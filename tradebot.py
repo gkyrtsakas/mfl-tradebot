956d198241d1514ec37a48fc697134f39b80012a
gsakis <gkyrtsakas@gmail.com>
2020-04-05 13:06:28 -0400

    WIP Rewrite tradebot to use JSON

--- tradebot.py
+++ tradebot.py
@@ -1,129 +1,62 @@
-import xml.etree.ElementTree as ET
-import os
+import requests
+import json
 
-tradeparse = ET.parse('transactions.xml')
-trades = tradeparse.getroot()
+def https_request(url):
+    headers = {
+        'User-Agent': 'Tradebot_User_Agent'
+    }
 
-leagueparse = ET.parse('league.xml')
-league = leagueparse.getroot()
+    resp = requests.get(url, headers=headers)
 
-playerparse = ET.parse('players.xml')
-players = playerparse.getroot()
+    return resp.text
 
-rosterparse = ET.parse('rosters.xml')
-rosters = rosterparse.getroot()
+def get_rosters():
+    s = "https://www61.myfantasyleague.com/2020/export?TYPE=rosters&L=14095&JSON=1"
 
-try:
-	file = open("latest.trade", "r+")
-	ltrade = file.read(10)
-except:
-	file = open("latest.trade", "w")
-	file.close()
-	ltrade = 0
+    rosters_json = json.loads(https_request(s))
 
-def getFranchiseInfo(fnum):
-	for child in league:
-		if child.tag == "franchises":
-			f = child
-			for fchild in f:
-				if fchild.attrib['id'] == fnum:
-					if fchild.attrib['name'][0] == "<":
-						return "Franchise " + fchild.attrib['id']
-					else:
-						return fchild.attrib['name'][:20]
+    # print(rosters_json["encoding"])
 
-def tradesToProcess():
-	t = []
-	for child in trades:
-		if child.attrib['type'] == "TRADE":
-			if ltrade < child.attrib['timestamp']:
-				t.append(child.attrib['timestamp'])
-	return t
+    pretty_text = json.dumps(rosters_json, indent=4, sort_keys=True)
 
-def infoFromID(id):
-	s = ""
-	for child in players:
-		if child.attrib['id'] == str(id):
-			if child.attrib['name'] == '':
-				return "An amnesty"
-			s += child.attrib['team'] + " "
-			s += child.attrib['position'] + " "
-			s += child.attrib['name'].split(', ')[1] + " "
-			s += child.attrib['name'].split(', ')[0] + " "
-			s = s.replace("'","\\u0027")
-			s = "".join(i for i in s if ord(i)<128)
+    # print(resp.text)
 
-	for child in rosters:
-		for plyr in child:
-			if plyr.attrib['id'] == id:
-				s += "($" + plyr.attrib['salary']
-				s += ", " + plyr.attrib['contractYear'] + " yrs)"
-
-	return s
-
-def parseGive(gave):
-	print gave
-	if len(gave) == 0:
-		print "derp"
-		return
-	if gave[0] == 'D': #this years pick
-		if int(gave.split('_')[2]) < 9:
-			return "%s.0%s" % (str(int(gave.split('_')[1]) + 1), str(int(gave.split('_')[2]) + 1))
-		else:
-			return "%s.%s" % (str(int(gave.split('_')[1]) + 1), str(int(gave.split('_')[2]) + 1))
-	elif gave[0] == 'F': #next years pick
-		return "%s (%s) %s" % (gave.split('_')[3], gave.split('_')[2], getFranchiseInfo(gave.split('_')[1]).replace("'","\\u0027")[:16])
-	else: #player
-		return infoFromID(gave)
+    with open("rosters.json", "w") as text_file:
+        text_file.write(pretty_text)
 
 
-def processTrade(timestamp):
-	s = "**** TRADE ALERT ****\\n"
-	for child in trades:
-		if child.attrib['timestamp'] == timestamp:
-			f1 = child.attrib['franchise']
-			f2 = child.attrib['franchise2']
-			f1name = getFranchiseInfo(f1)
-			f2name = getFranchiseInfo(f2)
-			f1name = f1name.replace("'","\\u0027")
-			f1name = "".join(i for i in f1name if ord(i)<128)
-			f2name = f2name.replace("'","\\u0027")
-			f2name = "".join(i for i in f2name if ord(i)<128)
-			f1gave = child.attrib['franchise1_gave_up'].split(',')
-			f2gave = child.attrib['franchise2_gave_up'].split(',')
-			f1gave.pop()
-			f2gave.pop()
-			s += f1name + " gave up\\n\\n"
-			for part in f1gave:
-				s += str(parseGive(part)) + "\\n"
-			s += "\\n" + f2name + " gave up\\n\\n"
-			for part in f2gave:
-				s += str(parseGive(part)) + "\\n"
-			return s
+def get_trades():
+    s = "https://www61.myfantasyleague.com/2020/export?TYPE=transactions&TRANS_TYPE=TRADE&L=14095&JSON=1"
 
-GROUPME_BOTID = ""
+    trades_json = https_request(s)
+    return trades_json
 
-def groupMe(s):
-	cmd = "curl -d '{\"text\" : \""
-	cmd += s
-	cmd += "\", \"bot_id\" : \""
-	cmd += GROUPME_BOTID
-	cmd += "\"}'"
-	cmd += " -H 'Content-Type: application/json' https://api.groupme.com/v3/bots/post"
-	print cmd
-	os.system(cmd)
+def trade_print(trade):
+    # print(trade)
+    print("TRADE")
+    print("%s gave up %s" % (trade["franchise"], trade["franchise1_gave_up"]))
+    print("%s gave up %s" % (trade["franchise2"], trade["franchise2_gave_up"]))
+    print("")
 
-if __name__ == '__main__':
-	t = tradesToProcess()
-	for tr in reversed(t):
-		s = processTrade(tr)
-		groupMe(s)
-	if len(t) > 0:
-		file.close()
-		file = open("latest.trade", "w")
-		print "TRADE WAS PROCESSED"
-		print t[0]
-		file.write(t[0])
-		file.close()
-	else:
-		file.close()
+def process_trades(trades_json_string):
+    trades_json = json.loads(trades_json_string)
+    trades = trades_json["transactions"]
+    # print(trades)
+    if (len(trades["transaction"]) == 1):
+        trade_print(trades["transaction"])
+    else:
+        print("hi mom")
+        print(trades["transaction"])
+        for trade in trades["transaction"]:
+            trade_print(trade)
+            
+
+
+
+def main():
+    # get_rosters()
+    process_trades(get_trades())
+
+
+if __name__ == "__main__":
+    main()
