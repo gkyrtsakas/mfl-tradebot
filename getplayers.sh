5e33850357925f544c057f5d409943ce5836eaa0
George Kyrtsakas <gkyrtsakas@gmail.com>
2020-04-29 21:11:48 -0400

    Remove unused scripts after rewrite

--- getplayers.sh
+++
@@ -1,7 +0,0 @@
-YEAR=2019
-LEAGUE=
-DIR=
-#### ONLY EDIT ABOVE THIS LINE
-/usr/bin/curl -L "www61.myfantasyleague.com/${YEAR}/export?TYPE=players&L=${LEAGUE}&W=&JSON=0" > ${DIR}/players.xml
-#a=`/bin/date`
-#/bin/echo "got players at $a" >> /root/cronlog
